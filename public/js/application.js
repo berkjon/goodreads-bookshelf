8512c964bbf41581dabd4949c4241da5e9175bf8
Jonathan Berk <jdsberk@gmail.com>
2015-03-10 23:07:56 -0700

    added flexbox to css styling

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 
-  $.ajax({
-    type: 'get',
-    url: 'https://www.goodreads.com/review/list/8403316.xml?key=ONBHGOyk3Zy1tq3meX1RZA&v=2'
-  }).done(function(xml){
-    // do work on xml response
-    debugger;
-  })
+
 
 });
